5c064dd4341a38eecfbe7a2432816db2de73e707
David <david@nlsn.nu>
2022-05-21 17:22:47 +0200

    working

--- index.js
+++ index.js
@@ -17,27 +17,46 @@ app.use(bodyParser.json());
 // Endpoints
 app.post("/", (req, res) => {
   console.log(req.body);
+
+	if(req.body.message === "undefined"){
+		return;
+	}
   const chatId = req.body.message.chat.id;
   const sentMessage = req.body.message.text;
 
-  // Regex for hello
-  if (sentMessage.match(/hello/gi)) {
-    axios
-      .post(`${url}${apiToken}/sendMessage`, {
-        chat_id: chatId,
-        text: "hello back 👋",
-      })
-      .then((response) => {
-        res.status(200).send(response);
-      })
-      .catch((error) => {
-        res.send(error);
-      });
-  } else {
-    // if no hello present, just respond with 200
-    res.status(200).send({});
+  console.log("Chatid: " + chatId + "\n sentMessage: " + sentMessage);
+
+  //Regex for hello
+  if (parseInt(chatId) > 1 && req.body.message !== "undefined") {
+    if (sentMessage.match(/hello/gi)) {
+      axios
+        .post(`${url}${apiToken}/sendMessage`, {
+          chat_id: chatId,
+          text: "hello back 👋",
+        })
+        .then((response) => {
+          res.status(200).send(response);
+        })
+        .catch((error) => {
+          res.send(error);
+        });
+    } else {
+      // if no hello present, just respond with 200
+      res.status(200).send({});
+    }
+  }
+
+  if (sentMessage.match(/fis/gi)) {
+    sendMessage(chatId, "ananas");
   }
 });
+
+async function sendMessage(chatId, message) {
+  axios.post(`${url}${apiToken}/sendMessage`, {
+    chat_id: chatId,
+    text: message,
+  });
+}
 
 // Listening
 app.listen(port, () => {
